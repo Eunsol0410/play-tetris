efe6db3d9dbd93c26cbf24ed750a263af60cfa98
eunsol0410 <eunsollee04@gmail.com>
2021-02-14 00:49:56 +0900

    [feat] 게임 설정 컴포넌트 구현
    - 설정(시작 레벨, 사운드, 다크모드)와 모달창 열림 닫힘 상태 갖도록 하였음

---
+++ src/components/Setting.ts
@@ -0,0 +1,66 @@
+enum theme {
+	dark = 'dark',
+	light = 'light'
+}
+
+interface settingType {
+	$target: HTMLElement;
+	startLevel: number;
+	setStartLevel: (level: number) => void;
+}
+
+export default class Setting {
+	readonly $setting: HTMLElement;
+	isOpened: boolean;
+	theme: theme;
+	sound: boolean;
+	level: number;
+	setStartLevel: (level: number) => void;
+
+	constructor({ $target, startLevel, setStartLevel }: settingType) {
+		this.$setting = document.createElement('div');
+		this.$setting.className = 'setting-container';
+		$target.append(this.$setting);
+
+		this.theme = window.matchMedia("(prefers-color-scheme: dark)").matches 
+			? theme.dark : theme.light;
+		document.documentElement.setAttribute('data-theme', this.theme);
+		
+		this.isOpened = false;
+		this.sound = false;
+		this.level = startLevel;
+		this.setStartLevel = setStartLevel;
+		this.render();
+	}
+
+	toggleIsOpened() {
+		this.isOpened = !this.isOpened;
+		if(this.isOpened) {
+			this.$setting.style.display = 'flex';
+		} else {
+			this.$setting.style.display = 'none';
+		}
+	}
+
+	render() {
+		const darkChecked = this.theme === theme.dark ? 'checked' : '';
+		const soundChecked = this.sound ? 'checked' : '';
+		this.$setting.innerHTML = `
+			<div class="setting">
+				<button class="close-btn">X</button>
+				<label for="theme">Dark Mode On </label>
+				<input class="theme" type="checkbox" name="theme" value="dark" ${darkChecked}>
+				<label for="sound">Sound On </label>
+				<input class="sound" type="checkbox" name="sound" value="on" ${soundChecked}>
+				<p>Level</p>
+				<section class="level-container">
+					<button class="levelUp-btn">▼</button>
+					<p class="level">${this.level}</p>
+					<button class="levelDown-btn">▲</button>
+				</section>
+			</div>
+		`;
+
+		if(!this.isOpened)	this.$setting.style.display = 'none';
+	}
+}
